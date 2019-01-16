87ea475749e6f4109a9f67bed1ca79f55f5266da
Sherin Binu <sherinbinu@hotmail.com>
2019-01-16 20:49:39 +0530

    use ORIGIN env variable

--- src/middlewares/set-response-headers.js
+++ src/middlewares/set-response-headers.js
@@ -1,5 +1,5 @@
 export default (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN);
   res.setHeader('Access-Control-Allow-Headers', 'content-type');
   next();
 };
