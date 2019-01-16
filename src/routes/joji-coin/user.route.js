a8006669d12f4f39aea48e0ceedc268c78408570
Sherin Binu <sherinbinu@hotmail.com>
2019-01-16 20:26:00 +0530

    signin on user creation

--- src/routes/joji-coin/user.route.js
+++ src/routes/joji-coin/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import userCtrl from '../../controllers/joji-coin/user.controller';
+import authCtrl from '../../controllers/joji-coin/auth.controller';
 
 const router = express.Router();
 
 router
   .route('/')
   .get(userCtrl.list)
-  .post(userCtrl.create);
+  .post(userCtrl.create, authCtrl.signin);
 
 export default router;
